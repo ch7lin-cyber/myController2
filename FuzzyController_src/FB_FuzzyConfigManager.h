6ae235892c956cad4d556aade873f68dbf376915
ch7lin-cyber <ch7.lin@gmail.com>
2026-08-09 15:55:38 +0800

    Add C++ compatibility guard to config manager

--- FuzzyController_src/FB_FuzzyConfigManager.h
+++ FuzzyController_src/FB_FuzzyConfigManager.h
@@ -1,264 +1,78 @@
 /******************************************************************************
- *
- * File:
- *      FB_FuzzyConfigManager.h
- *
- * Brief:
- *      Runtime Fuzzy Parameter Manager
- *
+ * File  : FB_FuzzyConfigManager.h
+ * Brief : Runtime Fuzzy Parameter Manager
  ******************************************************************************/
-
 #ifndef FB_FUZZY_CONFIG_MANAGER_H
 #define FB_FUZZY_CONFIG_MANAGER_H
-
 
 #include <stdint.h>
 #include <stdbool.h>
 
-
+//------------------------------------------------------------------------------------//
+// C++ compatibility // DO NOT DELETE
+#ifdef __cplusplus
+extern "C" {
+#endif
+//------------------------------------------------------------------------------------//
 
 #define FUZZY_CONFIG_VERSION      0x0200
-
-
-
 #define FUZZY_RULE_SIZE           49
-
-
-
 #define FUZZY_CONFIG_MAGIC        0x55AA
 
-
-
-
-
-/*
- * Membership Config
- */
-
-
 typedef struct
 {
-
     float Left;
-
     float Center;
-
     float Right;
-
     float Peak;
-
-
-}FuzzyMFConfig_t;
-
-
-
-
-/*
- * Scaling Config
- */
+} FuzzyMFConfig_t;
 
 typedef struct
 {
-
-
     float errorScale;
-
-
     float dErrorScale;
-
-
     float Ku;
-
-
-
-}FuzzyScalingConfig_t;
-
-
-
-
-
-
-/*
- * FeedForward Point
- */
-
+} FuzzyScalingConfig_t;
 
 typedef struct
 {
-
     float temperature;
-
-
     float pwm;
-
-
-}FuzzyFFConfig_t;
-
-
-
-
-
-
-
-/*
- * Complete Configuration
- */
+} FuzzyFFConfig_t;
 
 typedef struct
 {
-
-
     uint16_t magic;
-
-
     uint16_t version;
-
-
-
     bool enable;
-
-
-
-    /*
-     * 7 Membership
-     */
-
     FuzzyMFConfig_t MF[7];
-
-
-
-    /*
-     * 49 Rule
-     */
-
     uint8_t rule[7][7];
-
-
-
-    /*
-     * Scaling
-     */
-
     FuzzyScalingConfig_t scaling;
-
-
-
-    /*
-     * FeedForward
-     */
-
     FuzzyFFConfig_t ff[16];
-
-
     uint8_t ffSize;
-
-
-
-}FuzzyRuntimeConfig_t;
-
-
-
-
-
-/*
- * Runtime Manager
- */
+} FuzzyRuntimeConfig_t;
 
 typedef struct
 {
-
-
     FuzzyRuntimeConfig_t config;
-
-
-
     bool changed;
+} FB_FuzzyConfigManager_t;
 
+/* Forward declaration; full definition is supplied by FB_FuzzyController.h. */
+typedef struct FB_FuzzyController FB_FuzzyController_t;
 
+void FB_FuzzyConfig_Init(FB_FuzzyConfigManager_t *fb);
+void FB_FuzzyConfig_LoadDefault(FB_FuzzyConfigManager_t *fb);
+bool FB_FuzzyConfig_Check(FB_FuzzyConfigManager_t *fb);
+bool FB_FuzzyConfig_Apply(FB_FuzzyConfigManager_t *cfg, FB_FuzzyController_t *controller);
+bool FB_FuzzyConfig_SetRule(FB_FuzzyConfigManager_t *fb, uint8_t e, uint8_t de, uint8_t output);
+bool FB_FuzzyConfig_SetMF(FB_FuzzyConfigManager_t *fb, uint8_t index, FuzzyMFConfig_t *mf);
 
-}FB_FuzzyConfigManager_t;
-
-
-
-
-
-
-/*
- * Initialize
- */
-
-void FB_FuzzyConfig_Init(
-        FB_FuzzyConfigManager_t *fb);
-
-
-
-
-/*
- * Load default
- */
-
-void FB_FuzzyConfig_LoadDefault(
-        FB_FuzzyConfigManager_t *fb);
-
-
-
-
-
-/*
- * Validate
- */
-
-bool FB_FuzzyConfig_Check(
-        FB_FuzzyConfigManager_t *fb);
-
-
-
-
-
-
-/*
- * Apply configuration
- *
- * Send data into
- *
- * Fuzzy Controller
- *
- */
-
-bool FB_FuzzyConfig_Apply(
-        FB_FuzzyConfigManager_t *cfg,
-        FB_FuzzyController_t *controller);
-
-
-
-
-
-
-/*
- * Set Rule
- */
-
-bool FB_FuzzyConfig_SetRule(
-        FB_FuzzyConfigManager_t *fb,
-        uint8_t e,
-        uint8_t de,
-        uint8_t output);
-
-
-
-
-
-/*
- * Set Membership
- */
-
-bool FB_FuzzyConfig_SetMF(
-        FB_FuzzyConfigManager_t *fb,
-        uint8_t index,
-        FuzzyMFConfig_t *mf);
-
-
-
-
-
+//------------------------------------------------------------------------------------//
+// C++ compatibility
+#ifdef __cplusplus
+}
 #endif
+//------------------------------------------------------------------------------------//
+
+#endif /* FB_FUZZY_CONFIG_MANAGER_H */
