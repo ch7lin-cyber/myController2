6d5338725be1993f09ec37e6eedc7fb2f15f7d90
ch7lin-cyber <ch7.lin@gmail.com>
2026-08-09 17:31:18 +0800

    Branch2: add C++ linkage compatibility smoke test

---
+++ tests/cmake/test_fuzzy_cpp.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <cmath>
+#include <iostream>
+
+extern "C" {
+#include "FB_FuzzyController.h"
+#include "FB_FuzzyConfigManager.h"
+}
+
+int main()
+{
+    FB_FuzzyController_t controller{};
+    FB_FuzzyConfigManager_t config{};
+
+    FB_FuzzyController_Init(&controller);
+    FB_FuzzyConfig_Init(&config);
+
+    assert(FB_FuzzyConfig_Check(&config));
+
+    const float pwm = FB_FuzzyController_Run(&controller, 130.0f, 25.0f);
+
+    assert(std::isfinite(pwm));
+    assert(pwm >= controller.config.OutputMin);
+    assert(pwm <= controller.config.OutputMax);
+
+    std::cout << "C++ linkage smoke test: PASS (PWM=" << pwm << ")\n";
+    return 0;
+}
