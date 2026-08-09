68c91da9c12135c172ca84a2c3c412a70393fba7
ch7lin-cyber <ch7.lin@gmail.com>
2026-08-09 15:55:28 +0800

    Add C++ compatibility guard to hybrid output

--- FuzzyController_src/FB_FuzzyHybridOutput.h
+++ FuzzyController_src/FB_FuzzyHybridOutput.h
@@ -13,6 +13,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+//------------------------------------------------------------------------------------//
+// C++ compatibility // DO NOT DELETE
+#ifdef __cplusplus
+extern "C" {
+#endif
+//------------------------------------------------------------------------------------//
+
 #define FUZZY_HYBRID_FF_TABLE_SIZE  (16U)
 
 typedef struct
@@ -26,18 +33,14 @@ typedef struct
     float pwmMin;
     float pwmMax;
     float slewRate;
-
     FuzzyHybridFFPoint_t ffTable[FUZZY_HYBRID_FF_TABLE_SIZE];
     uint8_t ffSize;
-
     float neutralFuzzyPWM;
     float positiveCorrectionGain;
     float negativeCorrectionGain;
-
     float biasKi;
     float biasMin;
     float biasMax;
-
     bool enableFeedForward;
     bool enableBiasTrim;
     bool enableSlew;
@@ -60,20 +63,15 @@ typedef struct
 
 void FB_FuzzyHybridOutput_Init(FB_FuzzyHybridOutput_t *fb);
 void FB_FuzzyHybridOutput_Reset(FB_FuzzyHybridOutput_t *fb);
+bool FB_FuzzyHybridOutput_SetConfig(FB_FuzzyHybridOutput_t *fb, const FuzzyHybridOutputConfig_t *config);
+float FB_FuzzyHybridOutput_Run(FB_FuzzyHybridOutput_t *fb, float sv, float pv, float fuzzyPWM, float Ts);
+float FB_FuzzyHybridOutput_CalcFF(const FB_FuzzyHybridOutput_t *fb, float sv);
 
-bool FB_FuzzyHybridOutput_SetConfig(
-    FB_FuzzyHybridOutput_t *fb,
-    const FuzzyHybridOutputConfig_t *config);
-
-float FB_FuzzyHybridOutput_Run(
-    FB_FuzzyHybridOutput_t *fb,
-    float sv,
-    float pv,
-    float fuzzyPWM,
-    float Ts);
-
-float FB_FuzzyHybridOutput_CalcFF(
-    const FB_FuzzyHybridOutput_t *fb,
-    float sv);
+//------------------------------------------------------------------------------------//
+// C++ compatibility
+#ifdef __cplusplus
+}
+#endif
+//------------------------------------------------------------------------------------//
 
 #endif /* FB_FUZZY_HYBRID_OUTPUT_H */
